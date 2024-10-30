fecbe00632e4cb7608ad586a18c5460e519540b4
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-10-30 01:13:36 -0700

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -8,7 +8,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait
 from bot import Bot
 from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON
-from helper_func import encode
+from helper_func import *
 
 
 
