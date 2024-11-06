36ee94ce8d814afcb1a4a09f945975ba5f129baf
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-11-06 12:39:57 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -98,7 +98,7 @@ async def schedule_auto_delete(client, chat_id, message_id, delay):
     await client.delete_messages(chat_id=chat_id, message_ids=message_id)
     delete_tasks.delete_one({"chat_id": chat_id, "message_id": message_id})  # Remove from DB
     
-@Client.on_message(filters.command('start') & filters.private & subscribed)
+@Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
     id = message.from_user.id
     UBAN = BAN  # Fetch the owner's ID from config
